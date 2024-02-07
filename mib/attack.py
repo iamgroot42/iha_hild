Fix trace computation

mib/attack.py
@@ -26,7 +26,7 @@ def member_nonmember_loaders(
     train_data,
     train_idx,
     args,
-    num_train_points: int,
+    num_nontrain_pool: int = None,
     batch_size: int = 1,
     want_all_member_nonmember: bool = False,
 ):
@@ -44,15 +44,15 @@ def member_nonmember_loaders(
     # Sample non-members
     np.random.seed(args.exp_seed + 1)
     nonmember_indices = np.random.choice(
-        other_indices_train, num_train_points, replace=False
+        other_indices_train, num_nontrain_pool, replace=False
     )
 
     if want_all_member_nonmember:
         nonmember_index_subset = nonmember_indices
     else:
         # Break nonmember_indices here into 2 - one for sprinkling in FT data, other for actual non-members
-        nonmember_indices_ft = nonmember_indices[: num_train_points // 2]
-        nonmember_indices_test = nonmember_indices[num_train_points // 2 :]
+        nonmember_indices_ft = nonmember_indices[: num_nontrain_pool // 2]
+        nonmember_indices_test = nonmember_indices[num_nontrain_pool // 2 :]
 
         nonmember_dset_ft = ch.utils.data.Subset(train_data, nonmember_indices_ft)
 
@@ -103,7 +103,7 @@ def main(args):
     # TODO: Get these stats from a dataset class
     ds = get_dataset(args.dataset)(augment=False)
     # CIFAR
-    num_train_points = 10000
+    num_nontrain_pool = 10000
 
     # Get data
     train_data = ds.get_train_data()
@@ -118,21 +118,37 @@ def main(args):
         train_data,
         train_index,
         args,
-        num_train_points,
+        num_nontrain_pool,
     )
 
     # For reference-based attacks, train out models
     attacker = get_attack(args.attack)(target_model)
+
+    # Register trace if required
+    if attacker.requires_trace:
+        # Compute and add trace value to model
+        mem_loader_, nonmem_loader_ = member_nonmember_loaders(
+            train_data,
+            train_index,
+            args,
+            num_nontrain_pool=5000,
+            want_all_member_nonmember=True,
+            batch_size=256,
+        )
+        model_trace = compute_trace(target_model, mem_loader_, nonmem_loader_)
+        # Trace business (for theory-based attack)
+        attacker.register_trace(model_trace)
 
     if attacker.reference_based:
         ref_models, ref_indices = [], []
 
         if args.same_seed_ref:
             folder_to_look_in = os.path.join(model_dir, f"same_init/{args.target_model_index}")
-        elif args.specific_ref_folder is not None:
-            folder_to_look_in = os.path.join(model_dir, args.specific_ref_folder)
         else:
             folder_to_look_in = model_dir
+
+        if args.specific_ref_folder is not None:
+            folder_to_look_in = os.path.join(folder_to_look_in, args.specific_ref_folder)
 
         # Look specifically inside folder corresponding to this model's seed
         for m in os.listdir(folder_to_look_in):
@@ -141,8 +157,8 @@ def main(args):
                 continue
 
             # Skip ref model if trained on exact same data split as target model
-            if m.split(".pt")[0].split("_")[0] == f"{args.target_model_index}":
-                continue
+            # if m.split(".pt")[0].split("_")[0] == f"{args.target_model_index}":
+            #    continue
 
             model, _, _ = get_model(args.model_arch, 10)
             state_dict = ch.load(os.path.join(folder_to_look_in, m))
@@ -166,28 +182,24 @@ def main(args):
         # Shift model to CUDA (won't have ref-based models in memory as well)
         target_model.cuda()
 
-    """
-    # Compute and add trace value to model
-    model_trace = compute_trace(target_model, member_loader, nonmember_loader)
-    # Trace business (for theory-based attack)
-    attacker.register_trace(model_trace)
-    ref_traces = []
-    for m, ids in tqdm(
-        zip(ref_models, ref_indices), desc="Computing traces", total=len(ref_models)
-    ):
-        # Get in, out loaders corresponding to these models
-        mem_loader, nonmem_loader = member_nonmember_loaders(
-            train_data,
-            ids,
-            args,
-            num_train_points=num_train_points,
-            want_all_member_nonmember=True,
-            batch_size=256,
-        )
-        # Get traces corresponding to these models
-        ref_trace = compute_trace(m, mem_loader, nonmem_loader)
-        ref_traces.append(ref_trace)
-    """
+    if attacker.requires_trace:
+        ref_traces = []
+        for m, ids in tqdm(
+            zip(ref_models, ref_indices), desc="Computing traces", total=len(ref_models)
+        ):
+            # Get in, out loaders corresponding to these models
+            mem_loader, nonmem_loader = member_nonmember_loaders(
+                train_data,
+                ids,
+                args,
+                # num_train_points=num_train_points,
+                num_nontrain_pool=5000,
+                want_all_member_nonmember=True,
+                batch_size=256,
+            )
+            # Get traces corresponding to these models
+            ref_trace = compute_trace(m, mem_loader, nonmem_loader)
+            ref_traces.append(ref_trace)
 
     # Compute signals for member data
     signals_in, signals_out = [], []
@@ -195,18 +207,18 @@ def main(args):
     for x, y in tqdm(member_loader):
         out_models_use = None
         in_models_use = None
+        out_traces_use = None
         if attacker.reference_based:
-            out_models_use = [
-                ref_models[j] for j in np.nonzero(1 - member_map[:, i])[0][:]
-            ]
+            out_models_use = [ref_models[j] for j in np.nonzero(1 - member_map[:, i])[0][:]]
             in_models_use = [ref_models[j] for j in np.nonzero(member_map[:, i])[0]]
-            # out_traces_use = [ref_traces[j] for j in np.nonzero(1 - member_map[:, i])[0]]
             # in_traces_use = [ref_traces[j] for j in np.nonzero(member_map[:, i])[0]]
             if args.num_ref_models is not None:
                 out_models_use = out_models_use[: args.num_ref_models]
                 in_models_use = in_models_use[: args.num_ref_models]
-                # out_traces_use = out_traces_use[: args.num_ref_models]
                 # in_traces_use = in_traces_use[: args.num_ref_models]
+                if attacker.requires_trace:
+                    out_traces_use = [ref_traces[j] for j in np.nonzero(1 - member_map[:, i])[0]]
+                    out_traces_use = out_traces_use[: args.num_ref_models]
 
         score = attacker.compute_scores(
             x,
@@ -214,7 +226,7 @@ def main(args):
             out_models=out_models_use,
             in_models=in_models_use,
             other_data_source=nonmember_dset_ft,
-            # out_traces=out_traces_use,
+            out_traces=out_traces_use,
         )
         signals_in.append(score)
         i += 1
@@ -224,18 +236,18 @@ def main(args):
     for x, y in tqdm(nonmember_loader):
         out_models_use = None
         in_models_use = None
+        out_traces_use = None
         if attacker.reference_based:
-            out_models_use = [
-                ref_models[j] for j in np.nonzero(1 - nonmember_map[:, i])[0]
-            ]
+            out_models_use = [ref_models[j] for j in np.nonzero(1 - nonmember_map[:, i])[0]]
             in_models_use = [ref_models[j] for j in np.nonzero(nonmember_map[:, i])[0]]
-            # out_traces_use = [ref_traces[j] for j in np.nonzero(1 - nonmember_map[:, i])[0]]
             # in_traces_use = [ref_traces[j] for j in np.nonzero(nonmember_map[:, i])[0]]
             if args.num_ref_models is not None:
                 out_models_use = out_models_use[: args.num_ref_models]
                 in_models_use = in_models_use[: args.num_ref_models]
-                # out_traces_use = out_traces_use[: args.num_ref_models]
                 # in_traces_use = in_traces_use[: args.num_ref_models]
+                if attacker.requires_trace:
+                    out_traces_use = [ref_traces[j] for j in np.nonzero(1 - nonmember_map[:, i])[0]]
+                    out_traces_use = out_traces_use[: args.num_ref_models]
 
         score = attacker.compute_scores(
             x,
@@ -243,7 +255,7 @@ def main(args):
             out_models=out_models_use,
             in_models=in_models_use,
             other_data_source=nonmember_dset_ft,
-            # out_traces=out_traces_use,
+            out_traces=out_traces_use,
         )
         signals_out.append(score)
         i += 1

mib/attacks/base.py
@@ -13,14 +13,17 @@ class Attack(object):
     """
 
     def __init__(
-        self, name: str, model, whitebox: bool = False, reference_based: bool = False,
+        self, name: str, model, whitebox: bool = False,
+        reference_based: bool = False,
         label_smoothing: float = 0.0,
+        requires_trace: bool = False
     ):
         self.name = name
         self.model = model
         self.whitebox = whitebox
         self.reference_based = reference_based
         self.label_smoothing = label_smoothing
+        self.requires_trace = requires_trace
         self.criterion = ch.nn.CrossEntropyLoss(reduction="none", label_smoothing=self.label_smoothing)
 
     def compute_scores(self, x, y, **kwargs) -> np.ndarray:

mib/attacks/lira.py
@@ -57,7 +57,6 @@ class LiRAOnline(Attack):
     """
     Online verion of LiRA
     """
-
     def __init__(self, model, fix_variance: bool = True):
         super().__init__("LiRAOnline", model, reference_based=True)
         self.fix_variance = fix_variance
@@ -87,6 +86,81 @@ class LiRAOnline(Attack):
         for in_model in in_models:
             in_model.cuda()
             conf = compute_scaled_logit(in_model, x, y)
+            in_model.cpu()
+            confs_in.append(conf)
+        confs_in = np.array(confs_in)
+
+        # Enforce same number of in and out models
+        min_both = min(len(confs_in), len(confs_out))
+        confs_in = confs_in[:min_both]
+        confs_out = confs_out[:min_both]
+
+        # Original code uses median instead of mean, so we'll do that too
+        mean_in = np.median(confs_in, 1)
+        mean_out = np.median(confs_out, 1)
+
+        if self.fix_variance:
+            std_in = np.std(confs_in)
+            std_out = np.std(confs_out)
+        else:
+            std_in = np.std(confs_in, 1)
+            std_out = np.std(confs_out, 1)
+
+        scores = []
+        for oc in observed_conf:
+            pr_in = scipy.stats.norm.logpdf(oc, mean_in, std_in + 1e-30)
+            pr_out = scipy.stats.norm.logpdf(oc, mean_out, std_out + 1e-30)
+            score = pr_in - pr_out
+            # scores.append(score.mean(1))
+            scores.append(score.mean())
+        scores = np.array(scores)
+        return scores
+
+
+class LiRAOnlineSmooth(Attack):
+    """
+        Version of LiRA-Online that uses label-smoothing in loss computation.
+    """
+    def __init__(self, model, fix_variance: bool = True):
+        super().__init__(
+            "LiRAOnlineSmooth", model, reference_based=True, label_smoothing=5e-4
+        )
+        self.fix_variance = fix_variance
+
+    def _compute_score(self, model, x, y):
+        # Equivalent to log(p/1-p) where p is the probability of the true class
+        model.cuda()
+        loss = self.criterion(model(x.cuda()).detach(), y.cuda())
+        model.cpu()
+        eps = 1e-45
+        score = -ch.log(ch.exp(loss + eps) - 1)
+        return score.cpu().numpy()
+
+    @ch.no_grad()
+    def compute_scores(self, x, y, **kwargs) -> np.ndarray:
+        x, y = x.cuda(), y.cuda()
+        out_models = kwargs.get("out_models", None)
+        in_models = kwargs.get("in_models", None)
+        if out_models is None or in_models is None:
+            raise ValueError(
+                f"{self.name} requires out_models and in_models to be specified"
+            )
+        observed_conf = self._compute_score(self.model, x, y)
+
+        # Out-model confidence values
+        confs_out = []
+        for out_model in out_models:
+            out_model.cuda()
+            conf = self._compute_score(out_model, x, y)
+            out_model.cpu()
+            confs_out.append(conf)
+        confs_out = np.array(confs_out)
+
+        # In-model confidence values
+        confs_in = []
+        for in_model in in_models:
+            in_model.cuda()
+            conf = self._compute_score(in_model, x, y)
             in_model.cpu()
             confs_in.append(conf)
         confs_in = np.array(confs_in)

mib/attacks/unlearn.py
@@ -51,7 +51,7 @@ class Unlearning(Attack):
 
             # Fine-tune model
             model_.cuda()
-            model_, _, _ = train_model(
+            model_ = train_model(
                 model_,
                 criterion,
                 loader,
@@ -71,9 +71,9 @@ class Unlearning(Attack):
             )
 
             # For now, look at difference in gradient norms
-            score = np.linalg.norm(signals_after["grads"]) - np.linalg.norm(
-                signals_before["grads"]
-            )
+            score = np.linalg.norm(signals_after["grads"]) - np.linalg.norm(signals_before["grads"])
+            # Closer to zero -> more likely member
+            score = np.exp(-np.abs(score))
             scores.append(score)
 
         return np.array(scores)

mib/visualize.py
@@ -16,6 +16,8 @@ plt.rcParams["font.family"] = "Times New Roman"
 
 def main(args):
     signals_path = os.path.join(get_signals_path(), str(args.model_index))
+
+    info = {}
     for attack in os.listdir(signals_path):
         attack_name = attack.split(".")[0]
         if attack_name == "ReferenceSmooth_4_ref":
@@ -26,27 +28,23 @@ def main(args):
         signals_out = data["out"]
         total_labels = [0] * len(signals_out) + [1] * len(signals_in)
 
-        # best_auc = 0
-        # best_multiplier = 1.0
-        # # for multiplier in np.linspace(0.0, 1., 100):
-        # for multiplier in [1.]:
-        #     signals_in_ = signals_in[:, 0] - multiplier * signals_in[:, 1]
-        #     signals_out_ = signals_out[:, 0] - multiplier * signals_out[:, 1]
-
-        #     total_preds = np.concatenate((signals_out_, signals_in_))
-        #     fpr, tpr, thresholds = roc_curve(total_labels, total_preds)
-        #     roc_auc = auc(fpr, tpr)
-        #     if roc_auc > best_auc:
-        #         best_auc = roc_auc
-        #         best_multiplier = multiplier
-        # print("%.3f  | %.3f" % (best_multiplier, best_auc))
-        # exit(0)
-
         total_preds = np.concatenate((signals_out, signals_in))
         fpr, tpr, thresholds = roc_curve(total_labels, total_preds)
         roc_auc = auc(fpr, tpr)
         plt.plot(fpr, tpr, label="%s (AUC = %0.3f)" % (attack_name, roc_auc))
-        print("%s | AUC = %0.3f" % (attack_name, roc_auc))
+
+        tpr_at_low_fpr = lambda x: tpr[np.where(np.array(fpr) < x)[0][-1]]
+        info_ = {
+            "roc_auc": roc_auc,
+            "tpr@0.1fpr": tpr_at_low_fpr(0.1),
+            "tpr@0.01fpr": tpr_at_low_fpr(0.01)
+        }
+        info[attack_name] = info_
+
+        print(
+            "%s | AUC = %0.3f | TPR@0.1FPR=%0.3f | TPR@0.01FPR=%0.3f"
+            % (attack_name, roc_auc, info_["tpr@0.1fpr"], info_["tpr@0.01fpr"])
+        )
 
     # Make sure plot directory exists
     if not os.path.exists(args.plotdir):